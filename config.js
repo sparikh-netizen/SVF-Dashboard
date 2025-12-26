d9a10bf50edce048f65d1baa0f852d9e15924bb3
sparikh-netizen <sparikh@spicevillage.eu>
2025-12-26 16:48:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ const CONFIG = {
 };
 
 // Shopify API Configuration
-const SHOPIFY_CONFIG = {
-  storeUrl: 'spice-village-eu.myshopify.com',
-  accessToken: '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', // ← Your Shopify token
-  apiVersion: '2024-01'
+// const SHOPIFY_CONFIG = {
+  // storeUrl: 'spice-village-eu.myshopify.com',
+  // accessToken: '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', // ← Your Shopify token
+  // apiVersion: '2024-01'
 };
